e448b0e693bedb7e4f446b3eb89b509c903bf1f0
kevin.kim <kevin.kim@devncore.org>
2022-04-20 06:20:06 +0900

    Update

---
+++ src/car-selling/CarSelling/Kakaotalk/UI/Units/SendButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kakaotalk.UI.Units
+{
+    public class SendButton : Button
+    {
+        static SendButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SendButton), new FrameworkPropertyMetadata(typeof(SendButton)));
+        }
+    }
+}
