f2582ddc2ca862a447e4b3f96ddcbf9ae24c2cd3
kevin.kim <kevin.kim@devncore.org>
2022-04-20 05:28:34 +0900

    Update

---
+++ src/car-selling/CarSelling/Kakaotalk/App.cs
@@ -0,0 +1,21 @@
+﻿using Kakaotalk.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kakaotalk
+{
+    public class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var win = new MainWindow();
+
+            win.ShowDialog();
+        }
+    }
+}
---
+++ src/car-selling/CarSelling/Kakaotalk/Local/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DevNcore.UI.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakaotalk.Local.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+    }
+}
